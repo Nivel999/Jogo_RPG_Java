e4ade15a1712c271e21fe6b7e013ed2a580cc65c
Davi Oli <davioliveira1737@gmail.com>
2026-05-17 16:45:50 -0300

    Organização de arquivos

--- Main.java
+++
@@ -1,9 +0,0 @@
-package Jogo_RPG_Java;
-
-public class Main{
-    public static void main(String[] args) {
-        int a = 6;
-        int b = 8;
-        System.err.println(a+b);
-    }
-}
---
+++ app/Main.java
@@ -0,0 +1,10 @@
+package app;
+import personagens.Heroi;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        
+        Heroi heroi = new Heroi(string a);       
+    }
+}
